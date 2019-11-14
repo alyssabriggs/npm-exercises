6ddf7f7f6216dbade13a5ab5a18c3ac5e8b45b19
alyssalanae <briggs.l.alyssa@gmail.com>
2019-11-14 11:19:31 -0600

    Completed npm exercise

---
+++ src/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const $ = require('jquery');
+
+$(document).ready(function () {
+
+
+$('body').css('background-color', 'blue');
+
+
+function sayHello() {
+    console.log("Hello");
+}
+
+sayHello();
+
+
+});
